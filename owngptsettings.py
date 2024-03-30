7cf25e02c9ffa73f0651a865a2afec8951097518
Lợi Văn Lưu <loi.van.luu@mgm-tp.com>
2024-03-30 12:45:39 +0700

    adapt

--- owngptsettings.py
+++ owngptsettings.py
@@ -3,11 +3,11 @@
 Create ownChat  private gpt
 @author: Lawrence P.J.
 """
-persist_directory = ".\\chroma_db"
-source_directory = ".\\source_documents"
+persist_directory = "./chroma_db"
+source_directory = "./source_documents"
 embeddings_model_name = "sentence-transformers/all-mpnet-base-v2"
 collection_name = "collection_name"
-model_path = ".\\Models\\zephyr-7b-beta.Q4_0.gguf"
+model_path = "./zephyr-7b-beta.Q4_0.gguf"
 model_n_ctx = 5000
 model_type = "LlamaCpp"
 #model_type = "GPT4All"
